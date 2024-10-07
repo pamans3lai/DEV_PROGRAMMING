763afe34e3f4b1fc3ff8f093556d83553f3daf56
pamans3lai <muchtarihsan@gmail.com>
2024-10-07 16:20:10 +0700

    cheerytree

--- R/chronology.R
+++ R/chronology.R
@@ -1,6 +1,7 @@
 # cran.r-project.org/doc/manuals/r-patched/R-intro.html
 
-  Read: manual: chapter 1: 07 Oktober 2024
+Read: manual: chapter 1: 07 Oktober 2024 16:16
+
 
 
   Unread: Chapter 2 and so on
