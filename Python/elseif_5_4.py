d5ef9c97e2610923d876432c0b64747e1a1d449f
pamans3lai <muchtarihsan@gmail.com>
2024-10-19 03:48:45 +0700

    obsidian

---
+++ Python/elseif_5_4.py
@@ -0,0 +1,20 @@
+
+x = 28
+
+print('x < 0') if x < 0:
+elif x == 0:
+    print('x is zero')                 # If it's not true that x < 0, check if x == 0
+elif x == 1:
+    print('x == 1')                    # If it's not true that x < 0 and x != 0, check if x == 1
+else:
+    print('none of the above is true')
+
+name = "John"
+
+print(True) if name == "John":
+
+elif name != "John":
+    print(False)
+
+    # none of the above is true
+    # True
