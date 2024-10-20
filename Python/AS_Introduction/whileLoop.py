8e4515b951418241f9f2f9c9bb248efc7f70f4a9
pamans3lai <muchtarihsan@gmail.com>
2024-10-20 14:19:46 +0700

    role

---
+++ Python/AS_Introduction/whileLoop.py
@@ -0,0 +1,16 @@
+square = 1
+
+while square <= 10:
+    print(square)
+    square += 1
+    
+print("Finished")
+
+square = 0
+number = 1
+
+while number <= 9:
+    square = number ** 2
+    print(square)
+    number += 1
+
