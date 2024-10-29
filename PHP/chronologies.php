b607a751e9564dcbc716acc347000c44eda2b28c
pamans3lai <muchtarihsan@gmail.com>
2024-10-29 15:55:43 +0700

    php2

--- PHP/chronologies.php
+++ PHP/chronologies.php
@@ -7,3 +7,9 @@ Oct 17, 2024. 21:41
 
 evaluasi: lewat 11 hari tanpa membaca php
 plan: baca dokumentasi hari ni 
+
+Oct, 29, 2024. 15:54
+
+evaluasi: lewat 12 hari tanpa membaca PHP-enabled
+plan: baca dokumentasi per hari 10 menit tiap hari, bikin jadwal membaca 30 menit untuk tiap language
+
