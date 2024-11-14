da42528f841472c35a825377295c83a18af192e8
pamans3lai <muchtarihsan@gmail.com>
2024-11-14 07:10:41 +0700

    lifetime

--- Rust/book_lang/dangling_references.rs
+++ Rust/book_lang/dangling_references.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let r;
-
-    {
-        let x = 5;
-        r = &x;
-    }
-
+    let x = 5;
+    r = &x;
     println!("r: {r}");
 }
---
+++ Rust/book_lang/lifetime.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "hahaha";
+
+    let result = longest(string1.as_str(), string2);
+    println!("stirng terpanjang adalah {result}");
+}
