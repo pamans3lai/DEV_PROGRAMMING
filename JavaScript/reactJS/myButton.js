a1c8b8713c3f33ae870549890d27f6614471414e
pamans3lai <muchtarihsan@gmail.com>
2024-11-15 17:29:43 +0700

    react

---
+++ JavaScript/reactJS/myButton.js
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Selamat Datang di Aplikasiku</h1>
+      <MyButton />
+    </div>
+  );
+}
