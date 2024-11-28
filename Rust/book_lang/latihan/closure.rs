cd433087b552451b79951d1fa12c9191148685b2
pamans3lai <muchtarihsan@gmail.com>
2024-11-29 00:07:49 +0700

    klosur

--- Rust/book_lang/latihan/closure.rs
+++ Rust/book_lang/latihan/closure.rs
@@ -1,5 +1,10 @@
+// fn main() {
+//    let numbers = vec![1, 2, 3];
+//    let tutup = || println!("First: {},", numbers[24]);
+//   tutup();
+// }
+//
 fn main() {
-   let numbers = vec![1, 2, 3];
-   let tutup = || println!("First: {},", numbers[24]);
-  tutup();
+    let klosur = 8;
+    println!("{}", klosur);
 }
