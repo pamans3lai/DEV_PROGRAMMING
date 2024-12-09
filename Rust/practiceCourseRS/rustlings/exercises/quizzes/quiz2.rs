cd8bf29a42bb1c6bd4cde265d170dfab4bbd82a1
pamans3lai <muchtarihsan@gmail.com>
2024-12-10 05:24:48 +0700

    quiz2

--- Rust/practiceCourseRS/rustlings/exercises/quizzes/quiz2.rs
+++ Rust/practiceCourseRS/rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,31 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+
+    // pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+    //     let mut output = Vec::new();
+
+    //     for (string, command) in input {
+    //         let new_string = match command {
+    //             Command::Uppercase => string.to_uppercase(),
+    //             Command::Trim => string.trim().to_string(),
+    //             Command::Append(n) => string + &"bar".repeat(n),
+    //         };
+    //         output.push(new_string);
+    //     }
+    //     output
+    // }
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(string, command)| match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,7 +61,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
+    // use super::my_module::transformer_iter;
     use super::Command;
 
     #[test]
