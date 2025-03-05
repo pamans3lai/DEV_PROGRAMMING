32c65477770c4f08733d5c1b64b1c439b7441a3b
pamans3lai <muchtarihsan@gmail.com>
2025-03-05 21:16:49 +0700

    ruby commit
    commit

--- Ruby/chronolgy.rb
+++ Ruby/chronolgy.rb
@@ -4,3 +4,11 @@ menyelesaikan dokumentasi Ruby on Rails. lanjut praktik membuat backend dan fron
 today, Oct 27 2024
 belum ada progress apapun
 evaluasi: baca satu chapter untuk ruby today hingga jam 2024
+
+evaluasi:
+  sekarang tanggal 03/03/2025
+satu pekan sebelum 5 bulan terakhir kali pake ruby
+eeeedunnnnn!
+gimana mau maju kalau gaa ada progres kayak gini???
+solusi:
+  saatu hari 5 program. alokasi 1 jam ruby
