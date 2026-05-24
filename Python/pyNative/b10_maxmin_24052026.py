62e8534d2a8f34881485d75f9e683efc8b9d8cbc
pamans3lai <muchtarihsan@gmail.com>
2026-05-24 13:48:40 +0000

    minmax
    minmax

---
+++ Python/pyNative/b10_maxmin_24052026.py
@@ -0,0 +1,7 @@
+nums = [45, 2, 89, 12, 7]
+
+terbesar = max(nums)
+terkecil = min(nums)
+
+print(f"terbesar: {terbesar}")
+print(f"terkecil: {terkecil}")
---
+++ Python/pyNative/b11_removingduplicates_24052026.py
@@ -0,0 +1,6 @@
+data = [1, 2, 2, 3, 4, 4, 4, 5]
+list_unik = [1, 2, 3, 4, 5,]
+
+data_unik = list(set(data))
+
+print(f"List unik: {data_unik}")
